895b02376f40ba2635db5c8f0911d6cc1c63dd81
GeChenjia <1455081026@qq.com>
2021-01-11 23:56:33 +0800

    First commit.

---
+++ app/src/main/java/com/unnyweather/android/SunnyWeatherApplication.java
@@ -0,0 +1,18 @@
+package com.unnyweather.android;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+
+public class SunnyWeatherApplication extends Application {
+
+
+    public static  final String TOKEN = "IxWeFMW5urnKAl0w";
+    @SuppressLint("StaticFieldLeak")
+    static public Context context;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context =getApplicationContext();
+    }
+}
